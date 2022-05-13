6c0486ad6f0fa358529fca37893dcd82c5dd3ac1
Meredithhelipigeon <meredithxinru@gmail.com>
2022-05-12 21:27:38 -0400

    Edit

--- Python_Cheat_Sheet.md
+++ Python_Cheat_Sheet.md
@@ -1,3 +1,11 @@
 # Python Cheat Sheet
 
 ## 1. Copy
+
+- `copy.copy(x)` return a shallow copy of x
+- `copy.deepcopy(x) `return a deep copy of x
+
+Interesting finding:
+
+- `x[:]` will returns a slicing window, which will do almost the same as `copy.deepcopy(x)` but quite fast!
+
--- interview/backtrack/permutations.py
+++ interview/backtrack/permutations.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def permute(self, nums):
         """
         :type nums: List[int]
@@ -12,4 +12,26 @@ class Solution(object):
         for i in list(perm):
             ret.append(list(i))
         
+        return ret
+
+class Solution2(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        last=len(nums)-1
+        ret=[]
+        
+        def permutation(cur,l):
+            if cur==last:
+                ret.append(l[:])
+            for i in range(last-cur+1):
+                # swap pos i+cur and pos cur
+                l[cur], l[i+cur] = l[i+cur], l[cur]
+                permutation(cur+1,l)
+                # swap back pos i+cur and pos cur
+                l[cur], l[i+cur] = l[i+cur], l[cur]
+        
+        permutation(0,nums)
         return ret
